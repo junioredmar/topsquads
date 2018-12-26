9162d24bf8c1476046548194ccd30130a61e92c6
Edmar Junior <edmar@ciandt.com>
2018-12-26 14:52:21 -0200

    Add properties to model

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,4 +1,17 @@
 export class Player {
     id: number;
     name: string;
+    price: string;
+    color: string;
+    rating: string;
+    nationImg: string;
+    clubImg: string;
+    imageUrl: string;
+    pace: number;
+    shooting: number;
+    passing: string;
+    dribbling: string;
+    defending: string;
+    physicality: string;
+    position: string;
 }
